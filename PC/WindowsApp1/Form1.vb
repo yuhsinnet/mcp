10db7dbc6c17e9d2ff988d31da76f058fe06e075
x78701542000 <x78701542000@livemail.tw>
2019-07-18 18:29:43 +0800

    初次建置

---
+++ PC/WindowsApp1/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports EasyModbus
+
+
+Public Class Form1
+
+    Dim cc As ModbusClient
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cc = New ModbusClient("com3")
+        cc.LogFileFilename = "C:\Users\SG\Source\gitProject\mcp\PC\WindowsApp1\modbus_log.txt"
+        cc.Disconnect()
+
+        cc.Baudrate = 38400
+
+        cc.Connect()
+
+        TextBox1.Text = cc.ReadInputRegisters(0, 1)(0).ToString()
+    End Sub
+End Class
