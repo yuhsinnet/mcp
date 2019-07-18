6faae929d06bcc5dace759f5831bec8857689596
x78701542000 <x78701542000@livemail.tw>
2019-07-18 22:06:27 +0800

    新增功能

--- PC/WindowsApp1/Form1.Designer.vb
+++ PC/WindowsApp1/Form1.Designer.vb
@@ -22,23 +22,55 @@ Partial Class Form1
     '請勿使用程式碼編輯器進行修改。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Connect_But = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Location = New System.Drawing.Point(142, 188)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(286, 22)
         Me.TextBox1.TabIndex = 0
         '
+        'Connect_But
+        '
+        Me.Connect_But.Location = New System.Drawing.Point(434, 187)
+        Me.Connect_But.Name = "Connect_But"
+        Me.Connect_But.Size = New System.Drawing.Size(75, 23)
+        Me.Connect_But.TabIndex = 1
+        Me.Connect_But.Text = "Connect"
+        Me.Connect_But.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 250
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(142, 160)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(286, 22)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(142, 216)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(286, 22)
+        Me.TextBox3.TabIndex = 3
+        '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(223, 40)
+        Me.Button1.Location = New System.Drawing.Point(515, 186)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 1
+        Me.Button1.TabIndex = 4
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
@@ -48,6 +80,9 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Connect_But)
         Me.Controls.Add(Me.TextBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -57,5 +92,9 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Connect_But As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
     Friend WithEvents Button1 As Button
 End Class
--- PC/WindowsApp1/Form1.vb
+++ PC/WindowsApp1/Form1.vb
@@ -9,21 +9,68 @@ Public Class Form1
 
 
 
+        cc = New ModbusClient("com3")
+        'cc.LogFileFilename = "C:\Users\SG\Source\gitProject\mcp\PC\WindowsApp1\modbus_log.txt"
+        cc.Disconnect()
+        cc.Baudrate = 38400
+        cc.Parity = System.IO.Ports.Parity.None
+        cc.StopBits = System.IO.Ports.StopBits.One
 
+
+
+    End Sub
+
+    Private Sub Connect_But_Click(sender As Object, e As EventArgs) Handles Connect_But.Click
+
+
+        If cc.Connected = False Then
+
+            cc.Connect()
+
+
+            TextBox1.Text = cc.ReadInputRegisters(0, 2)(0).ToString()
+            Dim HoReg() = cc.ReadHoldingRegisters(0, 2)
+
+            TextBox2.Text = HoReg(0).ToString()
+            TextBox3.Text = HoReg(1).ToString()
+
+
+
+            Timer1.Enabled = True
+
+            Connect_But.Text = "Close"
+
+        Else
+
+            Timer1.Enabled = False
+            System.Threading.Thread.Sleep(250)
+
+            cc.Disconnect()
+
+
+            Connect_But.Text = "Connect"
+        End If
+
+
+
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Enabled = False
+        TextBox1.Text = cc.ReadInputRegisters(0, 1)(0).ToString()
+        Timer1.Enabled = True
 
 
 
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        cc = New ModbusClient("com3")
-        cc.LogFileFilename = "C:\Users\SG\Source\gitProject\mcp\PC\WindowsApp1\modbus_log.txt"
-        cc.Disconnect()
 
-        cc.Baudrate = 38400
+        Dim Reg() As Integer = {CInt(TextBox2.Text), CInt(TextBox3.Text)}
 
-        cc.Connect()
+        cc.WriteMultipleRegisters(0, Reg)
 
-        TextBox1.Text = cc.ReadInputRegisters(0, 1)(0).ToString()
+
     End Sub
 End Class
