1f38c75929c4f3c67f7e950324b9419ae24aaef4
x78701542000 <x78701542000@livemail.tw>
2019-07-31 07:21:35 +0800

    使用AVCC 參考電壓

--- AVR/MCP/MCP.X/build/Release/production/_ext/1472/yaMBSiavr.o.d
+++ AVR/MCP/MCP.X/build/Release/production/_ext/1472/yaMBSiavr.o.d
@@ -5,7 +5,7 @@ build/Release/production/_ext/1472/yaMBSiavr.o.d \
  c:\program\ files\ (x86)\microchip\xc8\v2.05\avr\avr\include\inttypes.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.05\avr\lib\gcc\avr\5.4.0\include\stdint.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.05\avr\avr\include\stdint.h \
- C:/as7/7.0/Packs/Atmel/ATmega_DFP/1.3.300/include/avr/iom328p.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.05\avr\avr\include\avr\iom328p.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.05\avr\avr\include\avr\portpins.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.05\avr\avr\include\avr\common.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.05\avr\avr\include\avr\version.h \
@@ -24,7 +24,7 @@ c:\program\ files\ (x86)\microchip\xc8\v2.05\avr\lib\gcc\avr\5.4.0\include\stdin
 
 c:\program\ files\ (x86)\microchip\xc8\v2.05\avr\avr\include\stdint.h:
 
-C:/as7/7.0/Packs/Atmel/ATmega_DFP/1.3.300/include/avr/iom328p.h:
+c:\program\ files\ (x86)\microchip\xc8\v2.05\avr\avr\include\avr\iom328p.h:
 
 c:\program\ files\ (x86)\microchip\xc8\v2.05\avr\avr\include\avr\portpins.h:
 
--- AVR/MCP/main.c
+++ AVR/MCP/main.c
@@ -297,6 +297,9 @@ static void setup_adc(void)  // by ADC SETUP
 	/* AVcc as reference, ADC0 as input */
 	ADMUX =  0;
 
+    
+//    ADMUX = (1 << REFS0);
+    
 	/* Enable ADC and conversion complete interrupt. Trigger first conversion */
 	ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADATE);
 	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // 128 prescale
