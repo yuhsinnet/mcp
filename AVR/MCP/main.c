6e0d36b48a3f83b0063754c73280fbc4c40ba549
x78701542000 <x78701542000@livemail.tw>
2019-08-01 23:54:08 +0800

    重整格式

--- AVR/MCP/main.c
+++ AVR/MCP/main.c
@@ -38,7 +38,7 @@ volatile uint16_t holdingRegisters[4];
 volatile bool isTenTime = false;
 volatile uint16_t TenTime_sum;
 
-uint8_t  error_times;
+uint8_t error_times;
 
 uint16_t EEMEM myeeprom[4];
 
@@ -53,6 +53,7 @@ bool timer03_is_up;
 
 bool timer04_start;
 bool timer04_is_up;
+
 void time1ms_start(void) {
 
     TCCR2A |= (1 << WGM21);
@@ -100,14 +101,14 @@ ISR(TIMER2_COMPA_vect)//this ISR is called 1001.6025 times per second
     static uint16_t i;
     static uint16_t timer01_CV;
     static uint16_t timer02_CV;
-	static uint16_t timer03_CV;
-	static uint16_t timer04_CV;
+    static uint16_t timer03_CV;
+    static uint16_t timer04_CV;
 
     if (i++ > 500) {
         TOGGLE(B, 1);
         i = 0;
     }
-	// 延遲讀取計時器
+    // 延遲讀取計時器
     if (timer01_start == true) {
         if (timer01_CV++ > 5000) {
             timer01_is_up = true;
@@ -131,30 +132,30 @@ ISR(TIMER2_COMPA_vect)//this ISR is called 1001.6025 times per second
         timer02_is_up = false;
 
     }
-	    //////////////////////////////////////////////////////////////////////////錯誤延遲觸發計時器
-	    if (timer03_start == true) {
-		    if (timer03_CV++ > 5000) {
-			    timer03_is_up = true;
-			    timer03_CV = 0;
-		    }
-		    } else {
+    //////////////////////////////////////////////////////////////////////////錯誤延遲觸發計時器
+    if (timer03_start == true) {
+        if (timer03_CV++ > 5000) {
+            timer03_is_up = true;
+            timer03_CV = 0;
+        }
+    } else {
 
-		    timer03_CV = 0;
-		    timer03_is_up = false;
+        timer03_CV = 0;
+        timer03_is_up = false;
 
-	    }
-	    //////////////////////////////////////////////////////////////////////////錯誤重送間隔計時器
-	    if (timer04_start == true) {
-		    if (timer04_CV++ > 10000) {
-			    timer04_is_up = true;
-			    timer04_CV = 0;
-		    }
-		    } else {
+    }
+    //////////////////////////////////////////////////////////////////////////錯誤重送間隔計時器
+    if (timer04_start == true) {
+        if (timer04_CV++ > 10000) {
+            timer04_is_up = true;
+            timer04_CV = 0;
+        }
+    } else {
 
-		    timer04_CV = 0;
-		    timer04_is_up = false;
+        timer04_CV = 0;
+        timer04_is_up = false;
 
-	    }
+    }
 
 }
 
@@ -262,21 +263,17 @@ ISR(ADC_vect) {
     static uint8_t times;
 
 
-	if (isTenTime == false)
-	{
-	
+    if (isTenTime == false) {
 
-	if (++times > 20)
-	{
-		
-		isTenTime = true;
-		times = 0;
-	}
-	else
-	{
-		TenTime_sum = TenTime_sum + ADC;
-	}
-}
+
+        if (++times > 20) {
+
+            isTenTime = true;
+            times = 0;
+        } else {
+            TenTime_sum = TenTime_sum + ADC;
+        }
+    }
 
 
 
@@ -324,7 +321,7 @@ static void setup_adc(void) // by ADC SETUP
     ADMUX = 0;
 
 
-        ADMUX = (1 << REFS0);
+    ADMUX = (1 << REFS0);
 
     /* Enable ADC and conversion complete interrupt. Trigger first conversion */
     ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADATE);
@@ -356,53 +353,53 @@ int main(void) {
     sei();
     eeprom_read_block((void*) &holdingRegisters, (const void*) &myeeprom, 8);
 
-	_delay_ms(5000);
+    _delay_ms(5000);
     wdt_enable(7);
     while (1) {
 
 
-	if (isTenTime == true) //ADC 20次平均
-	{
-		 inputRegisters[0] =  TenTime_sum / 20;
-		 TenTime_sum = 0;
-		 isTenTime = false;
-		 
-	}
+        if (isTenTime == true) //ADC 20次平均
+        {
+            inputRegisters[0] = TenTime_sum / 20;
+            TenTime_sum = 0;
+            isTenTime = false;
+
+        }
 
         wdt_reset();
         modbusGet();
 
         static uint8_t error_cour_state = 0;
 
-		static bool is_setFire;
+        static bool is_setFire;
 
-		#define  clear_error 0
-		#define Over_coura_state 1
-		#define Down_coura_state 2
+#define  clear_error 0
+#define Over_coura_state 1
+#define Down_coura_state 2
 
-		#define error_times_limit 3
+#define error_times_limit 3
 
         if (((READ(D, 5) == LOW) | (timer04_is_up)) & (error_times < error_times_limit)) {
             error_cour_state = clear_error; //清除錯誤狀態
             CLEARp(outstate, 0);
-			CLEARp(outstate, 1);
-			CLEARp(outstate, 3);
-			               timer01_start = false;
-			               timer02_start = false;
-			               timer03_start = false;
-			               timer04_start = false;
-						   timer04_is_up = false;
-						   is_setFire = false;
+            CLEARp(outstate, 1);
+            CLEARp(outstate, 3);
+            timer01_start = false;
+            timer02_start = false;
+            timer03_start = false;
+            timer04_start = false;
+            timer04_is_up = false;
+            is_setFire = false;
         }
 
-		bool Over_coura = (inputRegisters[0] > holdingRegisters[0]); //過電流狀態
-		bool down_coura = (inputRegisters[0] < holdingRegisters[1]);// 過低電流狀態
+        bool Over_coura = (inputRegisters[0] > holdingRegisters[0]); //過電流狀態
+        bool down_coura = (inputRegisters[0] < holdingRegisters[1]); // 過低電流狀態
 
 
 
-        bool in_coura_zone = Over_coura & down_coura;// 在正常電流值
+        bool in_coura_zone = Over_coura & down_coura; // 在正常電流值
         //bool active_mode = (READ(D, 7) == LOW) & (READ(D, 6) == LOW); // 水塔水位觸發開關
-        bool active_mode = (READ(D,7) == LOW );// 水塔水位觸發開關(測試)
+        bool active_mode = (READ(D, 7) == LOW); // 水塔水位觸發開關(測試)
 
         if (error_cour_state == 0) {
 
@@ -412,36 +409,29 @@ int main(void) {
                 if (timer01_is_up == true) {// 開機送水延遲判讀
                     if (in_coura_zone == 0) {
 
-					timer03_start = true; // 異常狀態發生延後判讀
-					if (timer03_is_up)
-					{
-                        CLEARp(outstate, 3);
+                        timer03_start = true; // 異常狀態發生延後判讀
+                        if (timer03_is_up) {
+                            CLEARp(outstate, 3);
 
-                        if (Over_coura)
-                        {
-	                        error_cour_state = Over_coura_state;
+                            if (Over_coura) {
+                                error_cour_state = Over_coura_state;
+                            }
+                            if (down_coura) {
+                                error_cour_state = Down_coura_state;
+                            }
+                        } else {
+                            SETp(outstate, 0);
+                            SETp(outstate, 1);
                         }
-                        if (down_coura)
-                        {
-	                        error_cour_state = Down_coura_state;
-                        }
-					}
-					else
-					{
-						SETp(outstate, 0);
-						SETp(outstate, 1);
-					}
 
 
-                        
+
+                    } else {
+                        CLEARp(outstate, 0);
+                        CLEARp(outstate, 1);
+                        timer03_start = false;
+                        //error_times = 0;
                     }
-					else
-					{
-					            CLEARp(outstate, 0);
-					            CLEARp(outstate, 1);
-					timer03_start = false;
-					//error_times = 0;
-					}
 
                 }
 
@@ -449,45 +439,41 @@ int main(void) {
             } else {
                 CLEARp(outstate, 3);
                 timer01_start = false;
-				timer02_start = false;
-				timer03_start = false;
-				//timer04_start = false;
+                timer02_start = false;
+                timer03_start = false;
+                //timer04_start = false;
             }
 
 
         } else {
-		
-		if (is_setFire == false)
-		{
-		
-			error_times = error_times +1 ;
-		
-		is_setFire = true;
-		}
+
+            if (is_setFire == false) {
+
+                error_times = error_times + 1;
+
+                is_setFire = true;
+            }
 
 
-		if (error_times < error_times_limit)
-		{
-			timer04_start = true;
-		}
+            if (error_times < error_times_limit) {
+                timer04_start = true;
+            }
 
 
-        
-		    CLEARp(outstate, 3);
+
+            CLEARp(outstate, 3);
             timer01_start = false;
 
 
             timer02_start = true;
             if (timer02_is_up == true) {
 
-			if (error_cour_state == Over_coura_state)
-			{
-				TOGGLEp(outstate, 1);
-			}
-			if (error_cour_state == Down_coura_state)
-			{
-				TOGGLEp(outstate, 0);
-			}               
+                if (error_cour_state == Over_coura_state) {
+                    TOGGLEp(outstate, 1);
+                }
+                if (error_cour_state == Down_coura_state) {
+                    TOGGLEp(outstate, 0);
+                }
                 timer02_is_up = false;
                 timer02_start = false;
             }
